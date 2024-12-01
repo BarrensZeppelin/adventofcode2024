a6f38aeb3c3dcab7f7a8114a6449c9b91a093eb1
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2024-12-01 06:10:18 +0100

    Day 1

---
+++ 01.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env pypy3
+# 332/311
+from __future__ import annotations
+
+from util import *
+
+if sys.stdin.isatty() and len(sys.argv) == 1:
+    sys.stdin = open(__file__.replace("py", "in"))
+
+res = 0
+
+a, b = [], []
+for l in lines():
+    x, y = ints(l)
+    a.append(x)
+    b.append(y)
+
+# C1 = Counter(a)
+C2 = Counter(b)
+
+# for x, y in zip(sorted(a), sorted(b)):
+#     res += abs(y - x)
+
+for x in a:
+    res += x * C2[x]
+
+
+print(res)
+# submit(1, res)
+submit(2, res)
+# prints(res)
